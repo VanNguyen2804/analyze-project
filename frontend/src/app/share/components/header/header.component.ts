324675aee0225793203d8a1cb4e73c93b6f3d5d5
VanNguyen2804 <dinhvan2804@gmail.com>
2026-09-20 19:31:49 +0700

    chore: fix compiler

--- frontend/src/app/share/components/header/header.component.ts
+++ frontend/src/app/share/components/header/header.component.ts
@@ -4,8 +4,7 @@ import { AnalyzeService } from 'src/app/core/services/analyze.service';
 
 @Component({
   selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  templateUrl: './header.component.html'
 })
 export class HeaderComponent {
   currentCategory: string = 'MEGA';
